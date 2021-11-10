8f853f68d7c6242bc624958038905f62e6a52f8b
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-11-10 14:37:39 -0500

    toggle reminder

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
   <Header title="Task Tracker"/>
-  <Tasks @delete-task="deleteTask" 
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" 
   :tasks="tasks"/>
 
 </div>
@@ -28,6 +28,9 @@ export default {
       this.tasks = this.tasks.filter((task) => task.id !== id)
 
       }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
     }
   },
   created() {
