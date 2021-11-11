199b9e8178fdedd3bb60a0667ad06ff1a34938f3
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-11-11 17:59:50 -0500

    vue router wip

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,51 @@
 <template>
-<div class="container">
-  <Header @toggle-add-task="toggleAddTask" title="Task Tracker"
-  :showAddTask="showAddTask"/>
-  <div v-show="showAddTask">
-      <AddTask @add-task="addTask"/>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
-  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" 
-  :tasks="tasks"/>
-
-</div>
 </template>
 
 <script>
 import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks,
-    AddTask
+    Footer,
   },
   data() {
     return {
-      tasks: [],
-      showAddTask: false
+      showAddTask: false,
     }
   },
   methods: {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
-    },
-    deleteTask(id) {
-      if (confirm('Are you sure?')) {
-      this.tasks = this.tasks.filter((task) => task.id !== id)
-
-      }
-    },
-    toggleReminder(id){
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
-    },
-    async fetchTasks() {
-      const res = await fetch('api/tasks')
-      const data = await res.json()
-      return data
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`)
-      const data = await res.json()
-      return data
-    },
-
   },
-  async created() {
-    this.tasks = await this.fetchTasks()
-  }
 }
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: 'Poppins', sans-serif;
 }
+
 .container {
   max-width: 500px;
   margin: 30px auto;
@@ -82,6 +55,7 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
+
 .btn {
   display: inline-block;
   background: #000;
@@ -95,12 +69,15 @@ body {
   font-size: 15px;
   font-family: inherit;
 }
+
 .btn:focus {
   outline: none;
 }
+
 .btn:active {
   transform: scale(0.98);
 }
+
 .btn-block {
   display: block;
   width: 100%;
